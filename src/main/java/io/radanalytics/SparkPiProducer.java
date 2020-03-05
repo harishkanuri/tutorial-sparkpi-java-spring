7c98d6a3d7774b01a95d04986a6214cb74fe3f97
harishkanuri <harish.kanuri@gmail.com>
2020-03-05 16:00:40 -0500

    sql

--- src/main/java/io/radanalytics/SparkPiProducer.java
+++ src/main/java/io/radanalytics/SparkPiProducer.java
@@ -9,6 +9,14 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class SparkPiProducer implements Serializable {
+
+    private static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String MYSQL_USERNAME = "expertuser";
+    private static final String MYSQL_PWD = "expertuser123";
+    private static final String MYSQL_CONNECTION_URL =
+            "jdbc:mysql://mysql:3306/employees?user=" + MYSQL_USERNAME + "&password=" + MYSQL_PWD;
+
+
     public String GetPi(int scale) {
         JavaSparkContext jsc = SparkContextProvider.getContext();
 
@@ -25,6 +33,27 @@ public class SparkPiProducer implements Serializable {
             double y = Math.random() * 2 - 1;
             return (x * x + y * y < 1) ? 1 : 0;
         }).reduce((integer, integer2) -> integer + integer2);
+
+
+        //Data source options
+        Map<String, String> options = new HashMap<>();
+        options.put("driver", MYSQL_DRIVER);
+        options.put("url", MYSQL_CONNECTION_URL);
+        options.put("dbtable",
+                    "(select transaction_type, concat_ws(' ', customer_first_name, customer_last_name) as full_name from payment_event) as payment_event");
+        //options.put("partitionColumn", "emp_no");
+        options.put("lowerBound", "100");
+        options.put("upperBound", "499");
+        //options.put("numPartitions", "10");
+
+        //Load MySQL query result as DataFrame
+        DataFrame jdbcDF = sqlContext.load("jdbc", options);
+
+        List<Row> transactionRows = jdbcDF.collectAsList();
+
+        for (Row transactionRow : transactionRows) {
+            LOGGER.info(transactionRow);
+        }
 
         String ret = "Pi is rouuuughly " + 4.0 * count / n;
 
