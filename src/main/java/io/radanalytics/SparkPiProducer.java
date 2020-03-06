f5d7c104ac58fba66c1cf31f0a5cdbde19f7e014
harishkanuri <harish.kanuri@gmail.com>
2020-03-05 21:19:27 -0500

    changes

--- src/main/java/io/radanalytics/SparkPiProducer.java
+++ src/main/java/io/radanalytics/SparkPiProducer.java
@@ -4,12 +4,12 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 //import org.apache.spark.sql.DataSet;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 
 public class SparkPiProducer implements Serializable {
@@ -20,13 +20,16 @@ public class SparkPiProducer implements Serializable {
     private static final String MYSQL_USERNAME = "root";
     private static final String MYSQL_PWD = "E7MlnA47t8jrBskm";
     private static final String MYSQL_CONNECTION_URL =
-            "jdbc:mysql://mysql:3306/payment?useSSL=false&allowPublicKeyRetrieval=true&user=" + MYSQL_USERNAME + "&password=" + MYSQL_PWD;
+            "jdbc:mysql://mysql:3306/payment";
 
     // private static final JavaSparkContext sc =
     //         new JavaSparkContext(new SparkConf().setAppName("SparkJdbcDs").setMaster("local[*]"));
 
     // private static final SQLContext sqlContext = new SQLContext(sc);
-
+    
+    private static final SparkSession sparkSession =
+            SparkSession.builder().master("local[*]").appName("Spark2JdbcDs").getOrCreate();
+    
     public String GetPi(int scale) {
         JavaSparkContext jsc = SparkContextProvider.getContext();
 
@@ -49,25 +52,32 @@ public class SparkPiProducer implements Serializable {
         Map<String, String> options = new HashMap<>();
         options.put("driver", MYSQL_DRIVER);
         options.put("url", MYSQL_CONNECTION_URL);
+        options.put("user", MYSQL_USERNAME);
+        options.put("password", MYSQL_PWD);
         options.put("dbtable",
                     "(select transaction_type, concat_ws(' ', customer_first_name, customer_last_name) as full_name from payment_event) as payment_event");
         //options.put("partitionColumn", "emp_no");
         //options.put("lowerBound", "100");
         //options.put("upperBound", "499");
         //options.put("numPartitions", "10");
-
-        SparkSession sparkSession =
-                SparkSession.builder().master("local[*]").appName("Spark2JdbcDs").getOrCreate();
         
-       
-        //SQLContext sqlContext = new SQLContext(jsc);
+        final Properties connectionProperties = new Properties();
+        connectionProperties.put("user", MYSQL_USERNAME);
+        connectionProperties.put("password", MYSQL_PWD);
+
+         //SQLContext sqlContext = new SQLContext(jsc);
 
         //Load MySQL query result as DataFrame
         //DataFrame jdbcDF = sqlContext.load("jdbc", options);
-        Dataset<Row> jdbcRows = sparkSession.read().format("jdbc").options(options).load();
+        //Dataset<Row> jdbcRows = sparkSession.read().format("jdbc").options(options).load();
 
-        //List<Row> transactionRows = jdbcDF.collectAsList();
-        List<Row> transactionRows = jdbcRows.collectAsList();
+        final String dbTable =
+                "(select transaction_type, concat_ws(' ', customer_first_name, customer_last_name) as full_name from payment_event) as payment_event";
+        Dataset<Row> jdbcDF = 
+                sparkSession.read().jdbc(MYSQL_CONNECTION_URL, dbTable, connectionProperties);
+        
+        List<Row> transactionRows = jdbcDF.collectAsList();
+        //List<Row> transactionRows = jdbcRows.collectAsList();
 
         for (Row transactionRow : transactionRows) {
             LOGGER.info(transactionRow);
